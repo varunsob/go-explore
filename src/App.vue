bd5e83d5f21b52af36e3f7b8906115c32147d24d
Varun Sobti <varun.sobti@paytm.com>
2019-07-14 02:40:10 +0530

    Initial Setup and Code
    The initial code and setup including location detection and fetching venues.

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <Header />
+    <FilterLayout />
+    <CardLayout v-if="lat && lon" :lat="lat" :lon="lon" />
+  </div>
+</template>
+
+<script>
+import CardLayout from './components/CardLayout';
+import FilterLayout from './components/FilterLayout';
+import Header from './components/Header';
+
+export default {
+  name: 'app',
+  components: {
+    CardLayout,
+    FilterLayout,
+    Header,
+  },
+
+  data() {
+    return {
+      error: '',
+      lat:'',
+      lon:''
+    }
+  },
+
+  created() {
+    this.getLocation();
+  },
+
+   methods: {
+    getLocation: function () {
+	    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.setPosition);
+	    } else {
+	      this.error = "Geolocation is not supported.";
+	    }
+    },
+	  setPosition: function(position) {
+		  this.lat = position.coords.latitude;
+		  this.lon = position.coords.longitude;
+	  }
+	}
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: "Fakt",sans-serif,Helvetica,Arial;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.segment-title {
+  color: #000;
+  font-size: 24px;
+  font-weight: 600;
+  margin: 20px;
+  text-align: left;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <img class="logo" alt="Explore" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  background: linear-gradient(45deg, #0ABF53 0%, #4EE5E5 100%);
+  height: 50px;
+}
+.logo {
+  margin: 9px 0 0 10px;
+  width: 32px;
+}
+</style>
